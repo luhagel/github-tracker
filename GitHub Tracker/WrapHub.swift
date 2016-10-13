58c2ed65af4cb077376c235471b8e0876cd3bd96
Luca Hagel <hagel.luca@gmail.com>
2016-10-12 20:42:33 -0700

    Add Pods, make sure basic API Call succeeds

---
+++ GitHub Tracker/WrapHub.swift
@@ -0,0 +1,26 @@
+//
+//  WrapHub.swift
+//  GitHub Tracker
+//
+//  Created by Luca Hagel on 10/12/16.
+//  Copyright © 2016 Luca Hagel. All rights reserved.
+//
+
+import Alamofire
+import SwiftyJSON
+
+class WrapHub {
+    static let baseUrl: String = "https://api.github.com"
+    static let url = baseUrl + "/users/luhagel"
+    static func apiCall() {
+        Alamofire.request(WrapHub.url).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print("JSON: \(json)")
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
---
+++ Pods/Target Support Files/Pods-GitHub TrackerTests/Pods-GitHub TrackerTests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_GitHub_TrackerTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_GitHub_TrackerTestsVersionString[];
+
